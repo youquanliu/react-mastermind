e301265b43bd20d430b1eb5c78618cc7007d8357
LIU FRANK <youquan.liu@hotmail.com>
2020-09-23 13:46:25 -0400

    done

---
+++ build/precache-manifest.c3c3cf68cdc1f98102cfd76d8ac246b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2563473b434cc4621a03f1ea6e56d3e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ac9dfa6c6867a4c57e0",
+    "url": "/static/css/main.36837d44.chunk.css"
+  },
+  {
+    "revision": "321cbacfb6a40a345f47",
+    "url": "/static/js/2.6716db09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6716db09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ac9dfa6c6867a4c57e0",
+    "url": "/static/js/main.25e4aabb.chunk.js"
+  },
+  {
+    "revision": "81683e2c3ed2811f2343",
+    "url": "/static/js/runtime-main.2dff0437.js"
+  }
+]);
